b8f3cd8aaa0192fbf7db275661d048a5900251c1
Alex Roberson <ajroberson321@gmail.com>
2022-05-16 18:10:25 -0700

    Time: 29 ms (37.03%), Space: 13.8 MB (17.67%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -4,9 +4,12 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        numDigits = int(''.join(str(i) for i in digits))  
+        numDigits = int(''.join(str(i) for i in digits)) 
+        
         newDigits = str(numDigits + 1)
+        
         ans = []   
+        
         for i in range(len(newDigits)):
             ans.append(int(newDigits[i]))
         
